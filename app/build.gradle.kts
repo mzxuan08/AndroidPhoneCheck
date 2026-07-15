26cc415a1d8a723883e160905284ccf472057ad1
xmkjw63 <mzxmc@qq.com>
2026-07-15 12:40:32 +0800

    fix: support Bluetooth status on Android 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.androidphonecheck.app"
         minSdk = 28
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
